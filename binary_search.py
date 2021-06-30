50f4e54ca9af9224b2045d5012c97662bfd94b33
rajibhossain8521 <rajibhossain8521@gmail.com>
2021-06-30 13:00:31 +0600

    binary search algorithm implementation done

---
+++ binary_search.py
@@ -0,0 +1,26 @@
+# Binary search algorithm implementation
+#   0   1   2   3   4   5   6   7
+# [10, 20, 30, 40, 50, 60, 70, 80]
+
+def binary_search(number_list, search_value):
+
+    low = 0
+    high = len(number_list)-1
+
+    while low <= high:
+
+        mid = (low + high) // 2
+
+        if number_list[mid] == search_value:
+            print('Great!!...Value found at index ', mid)
+            return
+        if number_list[mid] < search_value: # Search in right side
+            low = mid+1
+        if number_list[mid] > search_value: # Search in left side
+            high = mid-1
+
+    print('OOPS!!..Value not found in number list')
+
+
+number_list = [10, 20, 30, 40, 50, 60, 70, 80]
+binary_search(number_list, search_value=100)
