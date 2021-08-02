5b984c8a66170870871fc0583f808a8c37179ff2
rajibhossain8521 <rajibhossain8521@gmail.com>
2021-08-02 14:56:04 +0600

    Initialized

---
+++ quick_sort.py
@@ -0,0 +1,11 @@
+# Quick Sort Implementation with Pythn
+
+def partition():
+    pass
+
+def quick_sort(L, low, high):
+    pass
+
+if __name__ == '__main__':
+    number_list = [3, 6, 5, 1, 8, 4, 7, 2]
+    quick_sort(number_list, 0, len(number_list)-1)
