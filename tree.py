beef325b249aebb5a51678b6c5395fbbe013b995
rajibhossain8521 <rajibhossain8521@gmail.com>
2021-08-10 22:35:30 +0600

    Added Post-Order & In-Order function

--- tree.py
+++ tree.py
@@ -8,7 +8,6 @@
          / \     / \
         5  10   3   4
 
-    Left | Node | Right
 """
 class Node:
     def __init__(self, data):
@@ -18,7 +17,7 @@ class Node:
 
     def add_left(self, node):
         self.left = node
-    
+
     def add_right(self, node):
         self.right = node
 
@@ -29,25 +28,21 @@ def create_tree():
     nine = Node(9)
     two.add_left(seven)
     two.add_right(nine)
-
     one = Node(1)
     six = Node(6)
     seven.add_left(one)
     seven.add_right(six)
-
     five = Node(5)
     ten = Node(10)
     six.add_left(five)
     six.add_right(ten)
-
     eight = Node(8)
     nine.add_right(eight)
-
     three = Node(3)
     four = Node(4)
     eight.add_left(three)
     eight.add_right(four)
-
+    
     # return root node
     return two
 
@@ -56,11 +51,29 @@ def pre_order(node):
     if node.left:
         pre_order(node.left)
     if node.right:
-        pre_order(node.right)
+        pre_order(node.right) 
+
+def post_order(node):
+    if node.left:
+        post_order(node.left)
+    if node.right:
+        post_order(node.right)
+    print(node.data, end=' ')    
+
+def in_order(node):
+    if node.left:
+        in_order(node.left)
+    print(node.data, end=' ')
+    if node.right:
+        in_order(node.right)
 
 
 if __name__ == '__main__':
     tree = create_tree()
     print(tree.data)
-
+    
     pre_order(tree)
+    print('\n')
+    post_order(tree)
+    print('\n')
+    in_order(tree)
